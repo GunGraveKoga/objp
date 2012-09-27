a3d3f348d1f6aa544d75c0b3e76607190046feda
Virgil Dupras <hsoft@hardcoded.net>
2012-09-27 11:57:17 -0400

    v1.3.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ LONG_DESC = open('README', 'rt').read() + '\n\n' + open('CHANGES', 'rt').read()
 
 setup(
     name='objp',
-    version='1.2.1',
+    version='1.3.0',
     author='Hardcoded Software',
     author_email='hsoft@hardcoded.net',
     packages=['objp'],
